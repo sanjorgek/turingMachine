288fdc6fd00c147fda18414268601cb486f0e8e9
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2016-08-06 20:15:05 -0500

    Empty Stack acepter ready

--- src/Math/Model/Automaton/Stack.hs
+++ src/Math/Model/Automaton/Stack.hs
@@ -15,9 +15,11 @@ Stack Automaton
 module Math.Model.Automaton.Stack
 (
   Delta(..)
+  ,Key(..)
   ,liftD
 	,StackA(..)
   ,getInputAlphabet
+  ,getSigma
   ,getStackAlphabet
 ) where
 import           Data.Delta
@@ -35,11 +37,16 @@ symbol in stack head and returns next state and update stack
 type Delta a = (:->:) a (Either Symbol Epsilon, Symbol) Wd
 
 {-|
+A key for a delta.
+-}
+type Key a = (State a, (Either Symbol Epsilon, Symbol))
+
+{-|
 Takes a list of tuples and lift a Delta
 
 >>>let delta = liftD [(0,"(",'Z',0,"IZ"),(0,"",'Z',0,""),(0,"(",'I',0,"II"),(0,")",'I',0,"")]
 -}
-liftD::(Ord a) => [(a, Wd, Symbol, a, Wd)]-> Delta a
+liftD:: Ord a => [(a, Wd, Symbol, a, Wd)]-> Delta a
 liftD xs = let
     (as,bs,cs,ds,es) = unzip5 xs
     f = map Q
@@ -50,10 +57,12 @@ liftD xs = let
     rs = zip (f ds) es
   in Map.fromList (zip ks rs)
 
-nextDTuple :: (Ord a) => Delta a -> (State a, (Either Symbol Epsilon, Symbol)) -> (State a, Wd)
+nextDTuple :: Ord a => Delta a -> Key a -> (State a, Wd)
 nextDTuple dt k = if Map.member k dt then dt Map.! k else (QE,[])
 
--- |Stack machine only needs a delta, an init state and an initial symbol
+-- |Stack machine only needs a delta, an init state and an initial symbol.
+--
+-- This works for empty stack and final state acceptor
 data StackA a = Stack (Delta a) (State a) (Final a) Symbol deriving(Show, Eq)
 
 getSigma :: [Either a b] -> [a]
@@ -61,23 +70,45 @@ getSigma [] = []
 getSigma (Left x : xs) = x : getSigma xs
 getSigma (_:xs) = getSigma xs
 
-getInputAlphabet :: StackA t -> [Either Symbol Epsilon]
+getInputAlphabet :: StackA a -> [Either Symbol Epsilon]
 getInputAlphabet (Stack dn _ _ _) = (fst . unzip . getFirstParam) dn
 
-getStackAlphabet :: StackA t -> Wd
+getStackAlphabet :: StackA a -> [Symbol]
 getStackAlphabet (Stack dn _ _ _) = (snd . unzip . getFirstParam) dn
 
 cleanStacks::[(State a, Wd)]->[(State a, Wd)]
 cleanStacks [] = []
 cleanStacks ((QE, _):xs) = cleanStacks xs
-cleanStacks ((_, []):xs) = cleanStacks xs
 cleanStacks (x:xs) = x : cleanStacks xs
 
-aceptEmptyStack::[(a,Wd)]->Bool
+aceptEmptyStack::[(State a,Wd)]->Bool
 aceptEmptyStack = any aceptF1
   where
+    aceptF1 (QE,_) = False
     aceptF1 (_,[]) = True
     aceptF1 (_,_) = False
 
-checkWordByStack (Stack dn qi _ z0) = let
-  in True
+epsilonTrans:: Ord a => Delta a -> State a -> Symbol -> (State a, Wd)
+epsilonTrans dn q b = nextDTuple dn (q, (Right Epsilon, b))
+
+symbolTrans:: Ord a => Delta a -> State a -> Symbol -> Symbol -> (State a, Wd)
+symbolTrans dn q a b = nextDTuple dn (q, (Left a, b))
+
+mapEpsilon:: Ord a => Delta a -> [(State a, Wd)] -> [(State a, Wd)]
+mapEpsilon dn [] = []
+mapEpsilon dn ((q,b:bs):xs) = let
+    (p,ys) = epsilonTrans dn q b
+  in (p, ys++bs) : mapEpsilon dn xs
+
+mapSymbol:: Ord a => Delta a -> Symbol -> [(State a, Wd)] -> [(State a, Wd)]
+mapSymbol dn _ [] = []
+mapSymbol dn a ((q,b:bs):xs) = let
+    (p, ys) = symbolTrans dn q a b
+  in (p, ys++bs) : mapSymbol dn a xs
+
+checkWordByStack :: Ord a => StackA a -> Wd -> Bool
+checkWordByStack (Stack dn qi _ z0) ws = let
+    checkString [] _ = False
+    checkString stks [] = aceptEmptyStack stks || checkString ((cleanStacks . mapEpsilon dn) stks) []
+    checkString stks (a:as) = checkString ((cleanStacks . mapSymbol dn a) stks) as
+  in checkString [(qi,[z0])] ws
