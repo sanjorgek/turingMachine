68ddc88926f3724c9906adec421299f69db84617
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-02-26 13:50:23 -0600

    variant classes

--- test/FiniteTest.hs
+++ test/FiniteTest.hs
@@ -1,29 +1,87 @@
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 module Main where
 
+import qualified Data.Map                    as Map
 import qualified Data.Set                    as Set
 import           Data.State
 import           Math.Model.Automaton.Finite
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
+import           Test.Hspec.Variant
 import           Test.QuickCheck
 import           Test.QuickCheck.Variant
 
-instance (Arbitrary a) => Arbitrary (State a) where
-  arbitrary = do
+returnEnum = return . toEnum
+
+oneOfEnum = oneof . map returnEnum
+
+instance Variant () where
+  invalid = return ()
+  valid = return ()
+
+instance Variant Char where
+  invalid = oneOfEnum $ [0..31]++[127..1114111]
+  valid = oneOfEnum [32..126]
+
+instance (Arbitrary a) => Variant (State a) where
+  invalid = return QE
+  valid = do
     x <- arbitrary
-    (oneof . map return) [QE, Q x]
+    return $ Q x
+
+instance (Variant a) => Variant [a] where
+  valid = do
+    x <- valid
+    xs <- valid
+    (oneof . map return) [[], [x], x:xs]
+  invalid = do
+    x <- invalid
+    xs <- invalid
+    y <- valid
+    ys <- valid
+    (oneof . map return) [[x], x:xs, x:ys, y:xs]
+
+instance (Arbitrary a) => Arbitrary (State a) where
+  arbitrary = oneof [invalid, valid]
+
+instance (Variant a, Variant b) => Variant ((,) a b) where
+  invalid = do
+    x <- invalid
+    y <- invalid
+    z <- valid
+    w <- valid
+    (oneof . map return) [(x,y), (x,z), (w,y)]
+  valid = do
+    x <- valid
+    y <- valid
+    return (x, y)
+
+instance (Ord a, Variant a) => Variant (Set.Set a) where
+  invalid = do
+    xs <- invalid
+    return $ Set.fromList xs
+  valid = do
+    xs <- valid
+    (oneof . map return) [Set.empty, Set.fromList xs]
+
+instance (Ord k, Variant k, Variant a) => Variant (Map.Map k a) where
+  invalid = do
+    xs <- invalid
+    return $ Map.fromList xs
+  valid = do
+    xs <- valid
+    (oneof . map return) [Map.empty, Map.fromList xs]
 
 instance (Ord a,Arbitrary a) => Variant (FiniteA a) where
   invalid = do
-    nd <- arbitrary
-    sqf <- arbitrary
-    q0 <- arbitrary
+    nd <- valid
+    sqf <- valid
+    q0 <- valid
     return $ FN nd sqf q0
   valid = do
-    d <- arbitrary
-    sqf <- arbitrary
-    q0 <- arbitrary
+    d <- valid
+    sqf <- valid
+    q0 <- valid
     return $ F d sqf q0
 
 instance (Ord a, Arbitrary a) => Arbitrary (FiniteA a) where
@@ -43,15 +101,17 @@ finiteAut = describe "Finite automaton check" $
     checkString pairWord "11111" `shouldBe` False
     checkString pairWord "11011" `shouldBe` True
 
-reachTest = describe "Transform" $ do
-  prop "reachable" $
+transDetTest = describe "Transform" $ do
+  propValid "reachable" $
     \ af w -> checkString (reachableDelta (af::FiniteA Int)) w == checkString af w
-  prop "distinguishable" $
+  propValid "distinguishable" $
     \ af w -> checkString (distinguishableDelta (af::FiniteA Int)) w == checkString af w
-  prop "minimize" $
+  propValid "minimize" $
     \ af w -> checkString (minimizeFinite (af::FiniteA Int)) w == checkString af w
 
 
 main::IO ()
 main = hspec $
-  describe "Math.Model.Automaton.Finite" finiteAut
+  describe "Math.Model.Automaton.Finite" $ do
+    finiteAut
+    transDetTest
