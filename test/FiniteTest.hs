591ba20f898d9e8dea856128d27fbb21786e3496
Jorge Santiago Alvarez Cuadros <sanjorgek@ciencias.unam.mx>
2017-02-26 03:30:51 -0600

    transform test
    reach, distinguishable, minimize
    
    and travis settings

--- test/FiniteTest.hs
+++ test/FiniteTest.hs
@@ -7,6 +7,30 @@ import           Math.Model.Automaton.Finite
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
+import           Test.QuickCheck.Variant
+
+instance (Arbitrary a) => Arbitrary (State a) where
+  arbitrary = do
+    x <- arbitrary
+    (oneof . map return) [QE, Q x]
+
+instance (Ord a,Arbitrary a) => Variant (FiniteA a) where
+  invalid = do
+    nd <- arbitrary
+    sqf <- arbitrary
+    q0 <- arbitrary
+    return $ FN nd sqf q0
+  valid = do
+    d <- arbitrary
+    sqf <- arbitrary
+    q0 <- arbitrary
+    return $ F d sqf q0
+
+instance (Ord a, Arbitrary a) => Arbitrary (FiniteA a) where
+  arbitrary = do
+    afn <- invalid
+    af <- valid
+    (oneof . map return) [afn, af]
 
 pairWord = F (liftDelta [(1,'0',1),(1,'1',2),(2,'0',2),(2,'1',1)]) (Set.fromList [Q 2]) (Q 2)
 
@@ -18,6 +42,14 @@ finiteAut = describe "Finite automaton check" $
     checkString pairWord "00001" `shouldBe` False
     checkString pairWord "11111" `shouldBe` False
     checkString pairWord "11011" `shouldBe` True
+
+reachTest = describe "Transform" $ do
+  prop "reachable" $
+    \ af w -> checkString (reachableDelta (af::FiniteA Int)) w == checkString af w
+  prop "distinguishable" $
+    \ af w -> checkString (distinguishableDelta (af::FiniteA Int)) w == checkString af w
+  prop "minimize" $
+    \ af w -> checkString (minimizeFinite (af::FiniteA Int)) w == checkString af w
 
 
 main::IO ()
